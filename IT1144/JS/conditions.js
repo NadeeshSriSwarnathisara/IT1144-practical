14ecda2e12e74904362c5aef776343a71729e093
NadeeshSriSwarnathisara <nadeeshsri2005swarnathisara@gmail.com>
2026-05-13 18:26:13 +0530

    Add files via upload

---
+++ IT1144/JS/conditions.js
@@ -0,0 +1,29 @@
+
+let age=20;
+ 
+ if (age >= 18){
+	 console.log (" you are an adult ");
+ }
+ console.log (" this always run ");
+ 
+ 
+ 
+  age=15;
+ 
+ if (age >= 18){
+	 console.log (" you are an adult ");
+ }
+ console.log (" this always run ");
+ 
+ 
+   age=20;
+ 
+ if (age >= 18){
+	 console.log (" Access granted welcome ");
+ }else
+ console.log ("Access denied you must be 18 or older ");
+ 
+ 
+ 
+ 
+ 
---
+++ IT1144/first.js
@@ -0,0 +1,60 @@
+console.log("My Name nadeesh");
+console.log(21);
+console.log("I am from Galle.");
+console.log("My degree programme is Bsc.IT");
+
+
+let studentName = "Nadeesh";
+let age = 21;
+let homeTown = "Galle";
+console.log(studentName);
+console.log(age);
+console.log(homeTown);
+
+
+ studentName = "Hansa";
+ age = 24;
+homeTown = "Badulla";
+console.log(studentName);
+console.log(age);
+console.log(homeTown);
+
+
+const pi= 3.14;
+console.log(pi);
+
+let city ='galle';
+console.log(city);
+
+let phoneNumber;
+console.log(phoneNumber);
+phoneNumber =0763929154;
+console.log(phoneNumber);
+phoneNumber ="0763929154";
+console.log(phoneNumber);
+
+
+
+let isStudent=true;
+console.log(isStudent);
+console.log(typeof isStudent);
+
+ age = 21;
+console.log(age);
+let total = 53;
+console.log(total);
+console.log(typeof age);
+console.log(typeof total);
+
+let a=20;
+let b=10;
+console.log(a+b);
+console.log(a-b);
+console.log(a/b);
+console.log(a*b);
+console.log(a**b);
+console.log(a%b);
+
+
+
+
